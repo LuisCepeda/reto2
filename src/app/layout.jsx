76f0669935df7c0bdcadb1a76f97a6fa237ea19e
Luis Cepeda <luissebastian.cepedaflechas@gmail.com>
2024-06-07 16:59:08 -0500

    google auth

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,12 @@
-import { Inter  as FontSans} from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/ui/navbar";
-
-const fontSans=FontSans({
+import { Providers } from './providers'
+const fontSans = FontSans({
   subsets: ['latin'],
-  variable:"--font-sans"
+  variable: "--font-sans"
 })
 
 export const metadata = {
@@ -18,21 +18,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}>
-        <ThemeProvider
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
+        <Providers>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-        >
-          <main className="container mx-auto">
-          <Navbar />
-            {children}
-
-          </main>
+          >
+            <main className="container mx-auto">
+              <Navbar />
+              {children}
+            </main>
           </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,14 +1,28 @@
+'use client'
 import Link from "next/link"
 import { ModeToggle } from "./theme-toggle-button"
-import { buttonVariants } from "./button"
+import { Button, buttonVariants } from "./button"
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Navbar() {
+
+    const { data: session } = useSession()
     return <>
-        <nav className="flex justify-between">
-            <Link href="/" className="text-3xl">Reto 2</Link>
-            <div className="flex gap-x-2 items-center ">
-                <Link href='/new' className={buttonVariants({variant:'secondary'})}>Crear Proyecto</Link>
-                <ModeToggle/> 
+        <nav className="flex justify-between items-center py-3 px-1 ">
+            <Link href="/" className="text-3xl">Taller 2 - SGPA</Link>
+
+
+            <div className="flex gap-x-2 items-center">
+                {session?.user ? (<>
+                    <Link href='/new' className={buttonVariants({ variant: 'secondary' })}>Crear Proyecto</Link>
+                    <p>{session.user.name}</p>
+                    <Button className={buttonVariants({ variant: 'secondary' })} onClick={async () => {
+                        await signOut({ callbackUrl: '/' })
+                    }
+                    }>Cerrar sesión</Button>
+                </>
+                ) : (<Button className={buttonVariants({ variant: 'secondary' })} onClick={() => signIn()}>Iniciar sesión</Button>)}
+                <ModeToggle />
             </div>
         </nav>
     </>
