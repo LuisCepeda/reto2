2c76694f29f0119991b318bddf9deded355da084
Luis Cepeda <luissebastian.cepedaflechas@gmail.com>
2024-06-18 14:04:27 -0500

    added function to make http requests

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export async function makeHttpRequest(endPoint,method,body) {
+    const url = `${process.env.BASE_URL}/api/${endPoint}`
+    
+    console.log('url', url)
+    try {
+        const response = await fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status},${await response.json()}`);
+        }
+
+        return await response.json()
+
+    } catch (error) {
+        console.error('Request failed', error);
+    }
+}
